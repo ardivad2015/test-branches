abd7d8eb4b72fa05f191f088380216a882434695
Ardivad <ardivad@gmail.com>
2024-06-14 02:30:34 +0300

    test start

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Test dev");
         System.out.println("finish");
+        System.out.println("start");
     }
 }
